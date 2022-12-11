37c667645d9f86db9ddf4ab5df1e537d265ba77e
rendyprayoga <93699796+rendyprayoga@users.noreply.github.com>
2022-12-11 18:54:38 +0700

    Update index.php

--- praktikum minggu 6/index.php
+++ praktikum minggu 6/index.php
@@ -1,6 +1,6 @@
 <?php
 
-function intToRoman($num)
+function int_to_roman($num)
 {
     $map = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
     $Value = '';
@@ -13,9 +13,9 @@ function intToRoman($num)
             }
         }
     }
-    return $Val;
+    return $Value;
 }
 
-echo(intToRoman(4))
+echo(int_to_roman(58))
 
 ?>
